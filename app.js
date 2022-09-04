4d4712d079e6bb8481bdb9f7c07243d8554e365f
Joe Gencarelli <jp.genc@gmail.com>
2022-09-04 14:31:03 -0400

    Created function to append c urrent coordinates to the DOM

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const geoB = document.querySelector('form');
 
+
+
+const showCords=(lat,long)=>{
+    const latitude = document.getElementById('lat');
+    const longitude = document.getElementById('long');
+    latitude.innerHTML=`Latitude: ${lat}`
+    longitude.innerHTML=`Longitude: ${long}`
+}
+
 const gotLocation = (pos) =>{
     const crd = pos.coords;
     console.log(pos);
     console.log('Your current position is:');
     console.log(`Latitude : ${crd.latitude}`);
     console.log(`Longitude: ${crd.longitude}`);
-    console.log(`More or less ${crd.accuracy} meters.`);
+   
+    let latitude=crd.latitude;
+    let longitude=crd.longitude
+
+    showCords(latitude,longitude)
   }
 
  const gotError=(err) =>{
-    console.warn(`ERROR(${err.code}): ${err.message}`);
+    console.log(`ERROR(${err.code}): ${err.message}`);
   }
 
 geoB.addEventListener('submit',(e)=>{
