4157a90af9f13ce2266b08d172d135316b65e05b
Joe Gencarelli <jp.genc@gmail.com>
2022-09-04 14:19:21 -0400

    Added functionality to get user's current cordinates

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("ciao world")
+const geoB = document.querySelector('form');
+
+const gotLocation = (pos) =>{
+    const crd = pos.coords;
+    console.log(pos);
+    console.log('Your current position is:');
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
+  }
+
+ const gotError=(err) =>{
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+geoB.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    navigator.geolocation.getCurrentPosition(gotLocation, gotError)
+})
